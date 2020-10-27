a2ec91b3967dfd6e27c2e47215b8bfe28e12342d
wagnerluzdevops <wagnerpfonseca@gmail.com>
2020-10-27 14:23:58 -0300

    Exemplo de conseguir info github com Ajax

---
+++ 09_98_Operadores_02.js
@@ -0,0 +1,19 @@
+const { XMLHttpRequest } = require('xmlhttprequest')
+const { ajax } = require('rxjs/ajax')
+const { map, concatAll } = require('rxjs/operators')
+
+ajax({
+    createXHR: () => new XMLHttpRequest(),
+    url: 'https://api.github.com/users/cod3rcursos/repos'
+})
+.pipe(
+    map(resp => JSON.parse(resp.xhr.responseText)),
+    concatAll(),
+    map(repo => repo.full_name)
+   
+)
+.subscribe({
+    next(x) { console.log('Repositorios ' + x); },
+    error(err) { console.error('Algo aconteceu de errado: ' + err); },
+    complete() { console.log('Finalizado'); }
+})
