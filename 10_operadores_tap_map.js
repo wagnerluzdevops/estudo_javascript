2a0e2c4cf546c09f9e56ed9fd6e61a5681abb4d1
wagnerluzdevops <wagnerpfonseca@gmail.com>
2020-10-28 10:34:28 -0300

    Operadores .tap e .map

---
+++ 10_operadores_tap_map.js
@@ -0,0 +1,17 @@
+// RxJS v6+
+const { of } = require ('rxjs');
+const { tap, map } = require ('rxjs/operators');
+
+
+const source = of(1, 2, 3, 4, 5);
+// transparently log values from source with 'tap'
+const example = source
+.pipe(
+  tap(val => console.log(`BEFORE MAP: ${val}`)),
+  map(val => val + 10),
+  tap(val => console.log(`AFTER MAP: ${val}`))
+);
+
+//'tap' does not transform values
+//output: 11...12...13...14...15
+const subscribe = example.subscribe(val => console.log(val));
