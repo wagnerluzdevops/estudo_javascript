19a315e3b60237f5df6d09bb9bf680563b69a785
wagnerluzdevops <wagnerpfonseca@gmail.com>
2020-11-24 15:19:18 -0300

    incluida outras possib de funçao

--- 00_function_como_variavel.js
+++ 00_function_como_variavel.js
@@ -9,3 +9,9 @@ let potenciaDe2 = potencia(2)
 console.log(potenciaDe2(3))
 
 // console.log( potencia(2)(3) )
+
+const boaTarde = function () {
+    console.log('Boa Tarde');
+}
+
+// console.log(boaTarde());
